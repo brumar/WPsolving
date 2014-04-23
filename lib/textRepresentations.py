ffa9e3648fe39f39706400d6a671d1949b129121
brumar <martin.bruno.mail@gmail.com>
2014-04-23 18:36:01 +0200

    Better structure
    new class : QuantityDic  (replace old dic)
    new class : TextGoal (initialised with the expert goal as for
    TextInformation)
    better init for TextGoal and TextInformation, takes a object and not
    init them (Goal and Representation are object needed for init)

--- lib/textRepresentations.py
+++ lib/textRepresentations.py
@@ -1,5 +1,39 @@
 import operations
 
+class QuantityDic:
+
+    def __init__(self, realDic,startAsVoid=False):
+        self.dic={}
+        for key in realDic.keys():
+            self.dic[key]=[]
+            self.dic[key].append(realDic[key])
+            if startAsVoid:
+                self.erase(key);
+
+
+    def isMultiple(self, key):
+        return (len(self.dic[key])>0)
+
+    def get(self,key,multiple=False):
+        if multiple:
+            return self.dic[key]
+        else:
+            return self.dic[key].pop(0) # if an element has a priority, it's at the first position
+
+    def addValue(self,key,value,erase=False,priority=False):
+        if erase:
+            self.erase(key)
+        insertPosition=0
+        if not priority:
+            insertPosition=len(self.dic[key])
+        self.dic[key].insert(insertPosition,value)
+
+    def isUnknown(self, key):
+        return not self.dic[key] #simplest way to check if the list is empty
+
+    def erase (self,key):
+        self.dic[key]=[]
+
 class Quantity:
     def __init__(self,obj,value):
         self.object=obj
@@ -10,11 +44,13 @@ class Representation:
         self.quantity=quantity
         self.weight=weight
         self.phrase=phrase
-        self.comment=comment               
+        self.comment=comment
 
 class Text:
     def __init__(self):
         self.textInformations=[]
+    def getTextInformation(self,index):
+        return(self.textInformations[index])
     def addTextInformation(self,textInformation):
         self.textInformations.append(textInformation)
     def setGoal(self,goal):
@@ -22,16 +58,44 @@ class Text:
 
 class TextInformation:
     alternativeRepresentations=[]
-    def __init__(self,quantity,phrase="",weight=0):
-        self.expertRepresentation=Representation(quantity,weight,phrase)
-    def addAlternativeRepresentation(self,quantity,phrase="",comment="", weight=0):
-        self.alternativeRepresentations.append(Representation(quantity,weight,phrase,comment))
+    def __init__(self,representation):
+        self.expertRepresentation=representation
+
+    def addAlternativeRepresentation(self,representation):
+        self.alternativeRepresentations.append(representation)
+
+    def getExpertRepresentation(self):
+        return self.expertRepresentation
+
+    def getAlternativeRepresentations(self):
+        return self.alternativeRepresentations
 
 class Goal:
-    alternativeGoals=[]
     def __init__(self,obj,phrase="",weight=0):
         self.obj=obj
         self.phrase=phrase
         self.weight=weight
-    def addAlternativeGoal(self,obj,phrase="",comment="", weight=0):
-        self.alternativeGoals.append(Goal(obj,phrase,comment,weight))    
+
+class TextGoal:
+    alternativeGoals=[]
+    def __init__(self,goal):
+        self.expertGoal=goal
+
+    def addAlternativeGoal(self,goal):
+        self.alternativeGoals.append(goal)
+
+    def getExpertGoal(self):
+        return self.expertGoal
+
+    def getAlternativeGoals(self):
+        return self.alternativeGoals
+
+#===============================================================================
+# dict = {'Alice': "balbalbal" , 'Beth': '9102', 'Cecil': '3258'}
+# qd=QuantityDic(dict)
+# print(qd.isUnknown('Beth'))
+# print(qd.isUnknown('Alice'))
+# qd.add_value('Cecil', 'dadada',priority=True)
+# print(qd.isMultiple('Cecil'))
+# print(qd.get('Cecil', False))
+#===============================================================================
