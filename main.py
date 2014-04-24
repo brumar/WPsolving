27acf7050d1a8c7df65568c3f58435705d77d1ee
brumar <martin.bruno.mail@gmail.com>
2014-04-24 19:29:00 +0200

    classes TreePaths and Step
    Classes used to track back the history has been added

--- main.py
+++ main.py
@@ -4,6 +4,49 @@ from lib.schemas import *
 from lib.subjectRepresentations import *
 from lib.textRepresentations import *
 import copy
+import uuid
+
+
+class TreePaths:
+	def __init__(self,updater):
+		self.dicStep={}	# id to index
+		self.steps=[]
+		self.initialState=updater
+		self.threeOutput=""
+
+	def addStep(self, step):
+		self.steps.append(step)
+		self.dicStep[step.id]=len(self.steps)
+		if (step.parentId!=0): # if it is not the first step in the three
+			self.addChild(self.parentId,self.id)
+
+	def addChild(self,parentId,childId):
+		indexOfStep=self.dicStep[parentId]
+		parentStep=self.steps[indexOfStep]
+		parentStep.childrenIds.append(childId)
+
+	def getStep(self,sId):
+		return (self.steps[self.dicStep[sId]])
+
+	def printAsThree(self,sId=0,step=0): # to be use
+		firstStepId=self.dicStep[sId]
+		firstStep=self.getStep(firstStepId)
+		childrenIds=firstStep.childrenIds
+		for childrenId in childrenIds:
+			line=step*"\t"+str(childrenId)+"\r\n"
+			self.threeOutput+=line
+			step+=1
+			self.printAsThree(childrenId,step)
+
+
+
+class Step:
+	def __init__(self,move,parentId=0):
+		self.childrenIds=[]
+		self.move=move
+		self.parentId=parentId
+		self.sId=uuid.uuid4()
+
 
 
 class Solver:
@@ -11,6 +54,7 @@ class Solver:
 		self.history=[]
 		self.problem=problem
 		self.treeVIZ=""
+		self.TreePaths=TreePaths(Updater(self.problem))
 
 	def recurciveBlindForwardSolve(self,movelist=[],newmove="",step=0,history=[],schemeToApply=0):
 		if(step!=0):
@@ -22,7 +66,7 @@ class Solver:
 				newmove=Move(schem)
 				#print(step,s,ml,schem.positions.keys())
 				self.updateTree(step,schem.positions.keys())
-				self.recurcive_solve(copy.deepcopy(movelist),newmove,step+1,copy.deepcopy(history))
+				self.recurciveBlindForwardSolve(copy.deepcopy(movelist),newmove,step+1,copy.deepcopy(history))
 		else:
 			#print(history)
 			step=0
@@ -41,15 +85,15 @@ struct.addBridgingSchemas(schema1,schema2)
 struct.updateObjectList()
 
 text=Text()
-text.addTextInformation(TextInformation(Representation(Quantity("PoissonGAIN",5),'Au supermarché, le kilo de poisson a augmenté de 5 euros cette année')))
-text.addTextInformation(TextInformation(Representation(Quantity("PoissonEF",12),'Un kilo de poisson coûte maintenant 12 euros.')))
-text.addTextInformation(TextInformation(Representation(Quantity("PoissonEIminusViandeEI",0),'Au début de l\'année, le kilo de viande coûtait le même prix que le kilo de poisson.')))
-text.addTextInformation(TextInformation(Representation(Quantity("PoissonGAINminusViandeGAIN",3),'Le kilo de viande a augmenté de 3 euros de moins que le kilo de poisson')))
-text.setGoal(TextGoal(Goal('ViandeEF','Combien coûte le kilo de viande maintenant?')))
+text.addTextInformation(TextInformation(Representation(Quantity("PoissonGAIN",5),'Au supermarchÃ©, le kilo de poisson a augmentÃ© de 5 euros cette annÃ©e')))
+text.addTextInformation(TextInformation(Representation(Quantity("PoissonEF",12),'Un kilo de poisson coÃ»te maintenant 12 euros.')))
+text.addTextInformation(TextInformation(Representation(Quantity("PoissonEIminusViandeEI",0),'Au dÃ©but de l\'annÃ©e, le kilo de viande coÃ»tait le mÃªme prix que le kilo de poisson.')))
+text.addTextInformation(TextInformation(Representation(Quantity("PoissonGAINminusViandeGAIN",3),'Le kilo de viande a augmentÃ© de 3 euros de moins que le kilo de poisson')))
+text.setGoal(TextGoal(Goal('ViandeEF','Combien coÃ»te le kilo de viande maintenant?')))
 
-text.getTextInformation(0).addAlternativeRepresentation(Representation(Quantity("PoissonEI",5),'Au supermarché, le kilo de poisson était de 5 euros'))
-text.getTextInformation(0).addAlternativeRepresentation(Representation(Quantity("PoissonEF",5),'Au supermarché, le kilo de poisson coute 5 euros'))
-text.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("PoissonGAIN",3),'Le kilo de viande a augmenté de 3 euros'))
+text.getTextInformation(0).addAlternativeRepresentation(Representation(Quantity("PoissonEI",5),'Au supermarchÃ©, le kilo de poisson Ã©tait de 5 euros'))
+text.getTextInformation(0).addAlternativeRepresentation(Representation(Quantity("PoissonEF",5),'Au supermarchÃ©, le kilo de poisson coute 5 euros'))
+text.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("PoissonGAIN",3),'Le kilo de viande a augmentÃ© de 3 euros'))
 
 
 probleme1=Problem(struct,text)
