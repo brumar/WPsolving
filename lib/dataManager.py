8f6dcc093ddc59dc55d0b796d1137c1dfdcc064b
brumar <martin.bruno.mail@gmail.com>
2014-05-10 19:35:38 +0200

    new file to store simulation informations

---
+++ lib/dataManager.py
@@ -0,0 +1,34 @@
+import csv
+
+class SimulatedDatas: # gathering and printing informations accross the different solving models
+    def __init__(self):
+        self.datas={}
+
+    def addDataSet(self,pathList,problemName,solvingModel):
+        formulaDic=self.createFormulaDic(pathList)
+        self.createDataSet(formulaDic,problemName,solvingModel)
+
+    def createFormulaDic(self,pathList):
+        formulaDic={}
+        for path in pathList:
+            if path.formula not in formulaDic.keys():
+                formulaDic[path.formula]=[path]
+            else :
+                formulaDic[path.formula].append(path)
+        return formulaDic
+
+    def createDataSet(self,formulaDic,problem,model):
+        self.datas[problem,model]=[]
+        for key in formulaDic:
+            for path in formulaDic[key]:
+                line=[key,path.problemSolved,path.objectFormula,path.interpretationsSummary]
+                if line not in self.datas[problem,model]:
+                    self.datas[problem,model].append(line)
+                    print(line)
+
+    def printCSV(self):
+        with open('datas.csv', 'wb') as csvfile:
+            writer = csv.writer(csvfile, delimiter=';',quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            for problem,solvingModel in self.datas:
+                for line in self.datas[problem,solvingModel]:
+                    writer.writerow([problem]+[solvingModel]+line)
