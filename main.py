b36fc901c4acae26676a17f98a770c4af62d2b74
brumar <martin.bruno.mail@gmail.com>
2014-05-30 10:53:37 +0200

    Rewrite for simplicity
    - Function to generate all possibilities
    - Problem2 as a new version of probleme1 (check the ipynb)

--- main.py
+++ main.py
@@ -8,6 +8,26 @@ from lib.dataManager import *
 from lib.optionsFactory import *
 # analysis script 1
 
+
+def generateAllPossibilities(problem):
+    global simulatedDatas
+    c1=IntervalConstraint(['EF','EI'],operations.superiorOrEqualTo0)
+    upD=Updater(problem)
+    upD.startAsUnderstood()
+    print([Solver.INTERP,Solver.INTERP,Solver.SCHEMA,Solver.SCHEMA,Solver.SCHEMA])
+    optionsList=optionsFactory([Solver.INTERP,Solver.INTERP,Solver.SCHEMA,Solver.SCHEMA,Solver.SCHEMA]) # will generate all the options possible with 2 interpretations step randomly occuring
+    for i,options in enumerate(optionsList) :
+        model=str(options[0])
+        c2=BehavioralConstraint(breakTheOldOne=options[1])
+        constraints=[c1,c2]
+        solver=Solver(upD,constraints)
+        solver.generalSequentialSolver(listOfActions=options[0])
+        solver.TreePaths.scanTree()
+        #print(solver.TreePaths.treeOutput)
+        simulatedDatas.addDataSet(solver.TreePaths.pathList,probleme1.name,model)
+        print("DONE : model "+model+"( "+str(i)+" )")
+
+
 schema1=Schema("PoissonEF","PoissonEI",operations.addition,"PoissonGAIN","change")
 schema2=Schema("ViandeEF","ViandeEI",operations.addition,"ViandeGAIN","change")
 struct=ProblemStructure()
@@ -35,97 +55,25 @@ text.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity(
 text.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("PoissonEFminusViandeEF","-d"),'Le kilo de viande vaut 3 euros de plus que le kilo de poisson'))
 text.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("ViandeEF","d"),'Le kilo coute 3 euros à la fin'))
 probleme1=Problem(struct,text)
-probleme1.setInitialValues({"P1":5,"T1":12,"dEI":0,"d":3,"-d":-3})
 
+problem2=copy.deepcopy(probleme1)
+info3_prime=TextInformation(Representation(Quantity("PoissonEFminusViandeEF","d"),'Le kilo de viande vaut 3 euros de moins que le kilo de poisson'))
+info3_prime.addAlternativeRepresentation(Representation(Quantity("ViandeGAIN","d"),'Le kilo de viande a augmenté de 3 euros'))
+info3_prime.addAlternativeRepresentation(Representation(Quantity("ViandeGAIN","-d"),'Le kilo de viande a diminué de 3 euros'))
+info3_prime.addAlternativeRepresentation(Representation(Quantity("PoissonGAINminusViandeGAIN","-d"),'Le kilo de viande a augmenté de 3 euros de plus que le kilo de poisson'))
+info3_prime.addAlternativeRepresentation(Representation(Quantity("PoissonGAINminusViandeGAIN","d"),'Le kilo de viande a augmenté de 3 euros de moins que le kilo de poisson'))
+info3_prime.addAlternativeRepresentation(Representation(Quantity("PoissonEFminusViandeEF","-d"),'Le kilo de viande vaut 3 euros de plus que le kilo de poisson'))
+info3_prime.addAlternativeRepresentation(Representation(Quantity("ViandeEF","d"),'Le kilo coute 3 euros à la fin'))
+problem2.text.textInformations[3]=info3_prime
+problem2.text.setGoal(TextGoal(Goal('ViandeGAIN','De combien le kilo de viande a t-il augmenté ?')))
+problem2.name="Tc4p"
 
 simulatedDatas=SimulatedDatas()
+probleme1.setInitialValues({"P1":5,"T1":12,"dEI":0,"d":3,"-d":-3})
+problem2.setInitialValues({"P1":5,"T1":12,"dEI":0,"d":3,"-d":-3})
+generateAllPossibilities(problem2) # change simulated datas
 
-
-
-
-c1=IntervalConstraint(['EF','EI'],operations.superiorOrEqualTo0)
-upD=Updater(probleme1)
-upD.startAsUnderstood()
-print([Solver.INTERP,Solver.INTERP,Solver.SCHEMA,Solver.SCHEMA,Solver.SCHEMA])
-optionsList=optionsFactory([Solver.INTERP,Solver.INTERP,Solver.SCHEMA,Solver.SCHEMA,Solver.SCHEMA]) # will generate all the options possible with 2 interpretations step randomly occuring
-for i,options in enumerate(optionsList) :
-    model=str(options[0])
-    c2=BehavioralConstraint(breakTheOldOne=options[1])
-    constraints=[c1,c2]
-    solver=Solver(upD,constraints)
-    solver.generalSequentialSolver(listOfActions=options[0])
-    solver.TreePaths.scanTree()
-    #print(solver.TreePaths.treeOutput)
-    simulatedDatas.addDataSet(solver.TreePaths.pathList,"Tc4t",model)
-    print("DONE : model "+model+"( "+str(i)+" )")
-
-
-
-#===============================================================================
-# PROBLEM 2
-#===============================================================================
-#===============================================================================
-# text2=Text()
-# text2.addTextInformation(TextInformation(Representation(Quantity("PoissonGAIN","P1"),'Au supermarché, le kilo de poisson a augmenté de 5 euros cette année')))
-# text2.addTextInformation(TextInformation(Representation(Quantity("PoissonEF","T1"),'Un kilo de poisson coute maintenant 12 euros.')))
-# text2.addTextInformation(TextInformation(Representation(Quantity("PoissonEIminusViandeEI","dEI"),'Au début de l\'année, le kilo de viande coutait le même prix que le kilo de poisson.')))
-# text2.addTextInformation(TextInformation(Representation(Quantity("PoissonEFminusViandeEF","d"),'Le kilo de viande coûte maintenant 3 euros de moins que le kilos de poisson.')))
-# text2.setGoal(TextGoal(Goal('ViandeGAIN','De combien le kilo de viande a t-il augmenté ?')))
-#
-# text2.getTextInformation(0).addAlternativeRepresentation(Representation(Quantity("PoissonEI","P1"),'Au supermarché, le kilo de poisson était de 5 euros'))
-# text2.getTextInformation(0).addAlternativeRepresentation(Representation(Quantity("PoissonEF","P1"),'Au supermarché, le kilo de poisson coute 5 euros'))
-# text2.getTextInformation(1).addAlternativeRepresentation(Representation(Quantity("PoissonEI","T1"),'Un kilo de poisson était de 12 euros.'))
-# text2.getTextInformation(2).addAlternativeRepresentation(Representation(Quantity("PoissonEFminusViandeEF","dEI"),'Au la fin de l\'année, le kilo de viande coute le même prix que le kilo de poisson.'))
-# text2.getTextInformation(2).addAlternativeRepresentation(Representation(Quantity("PoissonGAINminusViandeGAIN","dEI"),'Le kilo de viande a augmenté du même prix que le kilo de poisson.'))
-# text2.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("ViandeGAIN","d"),'Le kilo de viande a augmenté de 3 euros'))
-# text2.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("ViandeGAIN","-d"),'Le kilo de viande a diminué de 3 euros'))
-# text2.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("PoissonGAINminusViandeGAIN","-d"),'Le kilo de viande a augmenté de 3 euros de plus que le kilo de poisson'))
-# text2.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("PoissonEFminusViandeEF","d"),'Le kilo de viande vaut 3 euros de moins que le kilo de poisson'))
-# text2.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("PoissonEFminusViandeEF","-d"),'Le kilo de viande vaut 3 euros de plus que le kilo de poisson'))
-# text2.getTextInformation(3).addAlternativeRepresentation(Representation(Quantity("ViandeEF","d"),'Le kilo coute 3 euros à la fin'))
-#
-# probleme2=Problem(struct,text2)
-# probleme2.setInitialValues({"P1":5,"T1":12,"dEI":0,"d":3,"-d":-3})
-#
-# upD=Updater(probleme2)
-# upD.startAsUnderstood()
-# c1=IntervalConstraint(['EF','EI'],operations.superiorOrEqualTo0)
-# c2=BehavioralConstraint(breakTheOldOne=True)
-# constraints=[c1,c2]
-#
-# solver=Solver(upD,constraints)
-# solver.generalSequentialSolver(listOfActions=[solver.SOLVER])
-# solver.TreePaths.scanTree()
-# simulatedDatas.addDataSet(solver.TreePaths.pathList,"Tc4p","Solve")
-#===============================================================================
-
-#===============================================================================
-# solver=Solver(upD,constraints)
-# solver.generalSequentialSolver(listOfActions=[solver.INTERP,solver.SCHEMA,solver.SOLVER])
-# solver.TreePaths.scanTree()
-# simulatedDatas.addDataSet(solver.TreePaths.pathList,"Tc4p","Int+Schem+Int+Solve_BREAK")
-# print(solver.TreePaths.treeOutput)
-# print(solver.TreePaths.pathsCount)
-#
-# solver=Solver(upD,constraints)
-# solver.generalSequentialSolver(listOfActions=[solver.INTERP,solver.INTERP,solver.SOLVER])
-# solver.TreePaths.scanTree()
-# simulatedDatas.addDataSet(solver.TreePaths.pathList,"Tc4p","Int+Int+Solve_BREAK")
-#
-# c2=BehavioralConstraint(breakTheOldOne=False)
-# constraints=[c1,c2]
-#
-# solver=Solver(upD,constraints)
-# solver.generalSequentialSolver(listOfActions=[solver.INTERP,solver.SCHEMA,solver.SOLVER])
-# solver.TreePaths.scanTree()
-# simulatedDatas.addDataSet(solver.TreePaths.pathList,"Tc4p","Int+Schem+Int+Solve_NOBREAK")
-#
-# solver=Solver(upD,constraints)
-# solver.generalSequentialSolver(listOfActions=[solver.INTERP,solver.INTERP,solver.SOLVER])
-# solver.TreePaths.scanTree()
-# simulatedDatas.addDataSet(solver.TreePaths.pathList,"Tc4p","Int+Int+Solve_NOBREAK")
-#===============================================================================
-print(simulatedDatas.printCSV())
+print(simulatedDatas.printCSV(hideUnsolved=True))
 
 
 
